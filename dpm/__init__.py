31dd3509a531843714afb732927c6b3512a8f902
benopp99 <50880489+benopp99@users.noreply.github.com>
2024-08-28 18:33:32 -0400

    Update __init__.py

--- dpm/__init__.py
+++ dpm/__init__.py
@@ -5,6 +5,6 @@ __author__ = 'Benjamin D. Oppenheimer'
 __credits__ = 'University of Colorado, Boulder'
 
 
-#from dpm.modelprofiles import (
-#    ModelMFlexGNFW,
-#    )
+from dpm.modelprofiles import (
+    ModelMFlexGNFW,
+    )
